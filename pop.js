10fc81218042a33e14b58751822c17d02d4fce3d
WaniShivani <wanishivani4@gmail.com>
2023-03-20 22:11:54 +0530

    async and await

---
+++ pop.js
@@ -0,0 +1,20 @@
+const getColddrink=async ()=>{
+
+const getbutter = new promise((resolve,reject)=>{
+    
+    setTimeout(()=> resolv("butter"),2000);
+});
+ }
+ {
+const getColddrink = new promise((resolve, reject)=>{
+setTimeout(()=> resolve("Colddrink"),3000);
+
+});
+ }
+ {
+
+let getbutter = await getbutter;
+console.log('coldrinks')
+console.log('butter')
+
+}
